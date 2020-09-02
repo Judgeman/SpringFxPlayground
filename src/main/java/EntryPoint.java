d6233dffec90155635349f6b676f84457c41e3eb
Paul Richter <paul@judgeman.de>
2020-09-02 15:08:01 +0200

    EntryPoint hinzugefügt Java Version hinzugefügt SpringFxPlayground hinzugefügt

---
+++ src/main/java/EntryPoint.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Paul Richter on Wed 02/09/2020
+ */
+public class EntryPoint {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt :)");
+    }
+}
---
+++ src/main/java/SpringFxPlayground.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Paul Richter on Wed 02/09/2020
+ */
+@SpringBootApplication
+public class SpringFxPlayground {
+
+
+}
